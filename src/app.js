14e06b3b6d99b8134048ae83010a2f31c7f795bc
Carol St.Louis <1.selva.oscura@gmail.com>
2017-03-11 14:20:31 -0800

    created entry point for app

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloWorld extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
+
+ReactDOM.render(<HelloWorld name="World" />, document.getElementById("root"));
