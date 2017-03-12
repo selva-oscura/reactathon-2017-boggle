c2bce5ab086c7c30017e85ea79f0bf3dfa36c8ae
Carol St.Louis <1.selva.oscura@gmail.com>
2017-03-11 17:23:30 -0800

    added hard-coded state data wordLists; imported and passed state.wordLists to WordLists;

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Button from './Button.js';
-import Board from './GameBoard/Board.js'
+import Board from './GameBoard/Board.js';
+import WordLists from './WordLists/WordLists';
 
 class App extends Component {
 	constructor(props){
@@ -11,24 +12,33 @@ class App extends Component {
 							  [ 'e', 'y', 'w', 'i' ],
 							  [ 'e', 'l', 'e', 's' ],
 							  [ 'o', 'p', 'o', 'p' ] 
-							]
+							],
+			wordLists: {
+				player1:{
+					words: ['bat', 'cat', 'tad']
+				}, 
+				player2:{
+					words:['bat', 'dog', 'whatever']
+				}
+			}
 		}
+		this.newGame = this.newGame.bind(this);
+	}
+	newGame () {
+		console.log('clicked for a new game!')
 	}
 	render(){
 		const board = this.state.board;
+		const wordLists = this.state.wordLists;
 		return (
 			<div className="App">
 				<Board board={board} />
 				<div name="Buttons">
-					<Button name={'delete'}>Delete</Button>
-					<Button name={'submit'}>Submit</Button>
+					<Button name={'delete'} />
+					<Button name={'submit'} />
+					<Button name={'new game'} newGame={this.newGame} />
 				</div>
-				<div name="Words">
-					<h4>Player 1</h4>
-					<p>words galore</p>
-					<h4>Player 2</h4>
-					<p>even more words galore</p>
-					</div>
+				<WordLists wordlists={wordLists} />
 			</div>
 		)
 	}
