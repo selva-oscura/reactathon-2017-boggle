5f041a11514221b6e069aceb53915110f35be5bf
Carol St.Louis <1.selva.oscura@gmail.com>
2017-03-11 17:22:11 -0800

    created WordLists component, set up PlayerWords and Word files;

---
+++ src/components/WordLists/WordLists.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const WordLists = ({wordLists}) => {
+	return (
+		<div name="Words">
+			<h4>Player 1</h4>
+			<p>words galore</p>
+			<h4>Player 2</h4>
+			<p>even more words galore</p>
+		</div>
+	);
+};
+export default WordLists;
+
